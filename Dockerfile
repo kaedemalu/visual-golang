a5572bcac4bd14fcd0c783efde52d99697275448
tito <tito@tito-MacBook-pro.local>
2019-07-16 15:29:34 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM golang:1.8-alpine3.9
---
+++ tetris.go
@@ -0,0 +1,47 @@
+module main
+
+import (
+  // add packages
+  "rand"
+  "time"
+  "math"
+  "github.com/go-gl/glfw/v3.2/glfw"
+)
+
+const (
+  BlockSize = 20
+  FieldHeight = 20
+  FieldWidth = 10
+  TetroSize = 4
+  WinWidth = BlockSize * FieldWidth
+  WinHeight = BlockSize * FieldHeight
+  TimerPeriod = 250 //ms
+)
+
+const (
+  BTetros = [
+          [66, 66, 66, 66],
+          [27, 131, 72, 232],
+          [36, 231, 36, 231],
+          [63, 132, 63, 132],
+          [311, 17, 223, 74],
+          [322, 71, 113, 47],
+          [1111, 9, 1111, 9],
+  ]
+  Colors = []
+)
+
+struct Block (
+  x int
+  y int
+)
+
+struct Game (
+  pos_x int
+  pos_y int
+)
+
+func main() {
+  // add script
+}
+
